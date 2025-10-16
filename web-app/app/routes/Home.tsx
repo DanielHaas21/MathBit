3db6f865a6679327dcdc2408b490393bdaa9e359
DanielHaas21 <daniel.haas@insio.cz>
2025-10-16 11:18:23 +0200

    fix

--- web-app/app/routes/Home.tsx
+++ web-app/app/routes/Home.tsx
@@ -1,2 +1,20 @@
+
 import { useEffect } from 'react';
 import { login } from 'web-api-client';
+
+export default function Home() {
+  useEffect(() => {
+    async function d() {
+      console.log(
+        await login(
+          { email: 'e.procházková4@example.com', password: 'test' },
+          { baseURL: 'http://localhost:3001' }
+        )
+      );
+    }
+
+    d();
+  }, []);
+
+  return <h1>Home Page</h1>;
+}
